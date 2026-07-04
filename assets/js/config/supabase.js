a03d3599f8d47a2c8e8fc31a635b0c5cdaaec8b6
dinda raras <hrdatamedia2024@gmail.com>
2026-07-04 15:34:54 +0700

    update

--- assets/js/config/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "sb_publishable_n9BDsGV-oOu6nbtQrcbYqg_VkSOPWCE";
-
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2emFmZHhyaHpsenZ1YmxveG1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNDQ1MjQsImV4cCI6MjA5ODcyMDUyNH0.w3TFFFMF5dr9GJpGQQn9OzSAU0dPcKUKCFUffOEXjdQ";
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
