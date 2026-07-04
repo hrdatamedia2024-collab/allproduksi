f2bdf13c0b81d01eb99b3b82a6a0d4dc7b1f8711
dinda raras <hrdatamedia2024@gmail.com>
2026-07-04 15:08:19 +0700

    perubahan ke 5

--- assets/js/config/supabase.js
+++ assets/js/config/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "hhttps://uvzafdxrhzlzvubloxml.supabase.co/rest/v1/";
+const SUPABASE_URL = "sb_publishable_n9BDsGV-oOu6nbtQrcbYqg_VkSOPWCE";
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2emFmZHhyaHpsenZ1YmxveG1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNDQ1MjQsImV4cCI6MjA5ODcyMDUyNH0.w3TFFFMF5dr9GJpGQQn9OzSAU0dPcKUKCFUffOEXjdQ";
 
